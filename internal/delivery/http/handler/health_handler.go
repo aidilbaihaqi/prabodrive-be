3072ba1f27a0d7f965f8aed0a9b9477333a5e72e
aidilbaihaqi <aidilmusirjun@gmail.com>
2026-06-02 13:11:42 +0700

    refactor: show maintenance info on health response

--- internal/delivery/http/handler/health_handler.go
+++ internal/delivery/http/handler/health_handler.go
@@ -1,11 +1,17 @@
 package handler
 
 import (
+	"os"
+
 	"github.com/gin-gonic/gin"
 
 	"github.com/aidilbaihaqi/prabodrive-be/internal/delivery/http/response"
 )
 
 func Health(c *gin.Context) {
-	response.OK(c, "ok", gin.H{"version": "1.0.0"})
+	isMaintenance := os.Getenv("MAINTENANCE_MODE") == "true"
+	response.OK(c, "ok", gin.H{
+		"version":        "1.0.0",
+		"is_maintenance": isMaintenance,
+	})
 }
